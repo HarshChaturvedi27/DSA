62da9bdf36bcf83cf936498440ce50d57ca9920b
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-03-06 00:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.36%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    long long helper (int n){
-        if (n==1) return 1;
-        return ((2*n - 1) + helper(n-1));
-    }
+    // long long helper (int n){
+    //     if (n==1) return 1;
+    //     return ((2*n - 1) + helper(n-1));
+    // }
     long long coloredCells(int n) {
-        if (n==1) return 1;
-        long long count = helper(n-1);
-        count = (2*n - 1) + 2 * count;
-        return count;
+    //     if (n==1) return 1;
+    //     long long count = helper(n-1);
+    //     count = (2*n - 1) + 2 * count;
+    //     return count;
+
+    return (long)2 * n * (n-1) + 1;
     }
+    
 };
