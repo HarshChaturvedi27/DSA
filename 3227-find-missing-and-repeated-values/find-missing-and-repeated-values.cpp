5cca9a0b7925c36306ea3c99446703121d8ea978
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-03-06 19:14:42 +0530

    Time: 13 ms (55.35%) | Memory: 31.5 MB (60.75%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -9,19 +9,24 @@ public:
                 hash[grid[i][j]]++;
             }
         }
-        for (auto& it : hash){
-            if (it.second == 2){
-                v.push_back(it.first);
-                break;
-            }
-        }
+        // for (auto& it : hash){
+        //     if (it.second == 2){
+        //         v.push_back(it.first);
+        //         break;
+        //     }
+        // }
+        int repeated = 0, missing = 0;
         for (int i=1; i<=n*n; i++){
-                if(hash.find(i) == hash.end()){
-                    v.push_back(i);
-                    break;
-                }
+            if(hash.find(i) == hash.end()){
+                missing = i;
+            }
+            if (hash[i] == 2){
+                repeated = i;
+            }
+            
         }
+        v.push_back(repeated);
+        v.push_back(missing);
         return v;
-        
     }
 };
