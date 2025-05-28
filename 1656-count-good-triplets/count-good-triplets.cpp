55d0c861421e787872e308fd0d2363520245702e
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-05-28 21:56:41 +0530

    Time: 7 ms (65.52%) | Memory: 11.1 MB (46.28%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -13,21 +13,30 @@ public:
         // }
         // return count;
 
+        // int count = 0;
+        // for (int i=0; i<arr.size()-2; i++){
+        //     for (int j=i+1; j<arr.size()-1; j++){
+        //         if (abs(arr[i] - arr[j]) <= a){
+        //             for (int k=j+1; k<arr.size(); k++){
+        //                 if ((abs(arr[j] - arr[k]) <= b) && (abs(arr[i] - arr[k]) <= c)){
+        //                     count++;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+
         int count = 0;
-        for (int i=0; i<arr.size(); i++){
-            for (int j=i+1; j<arr.size(); j++){
-                if (abs(arr[i] - arr[j]) <= a){
-                    for (int k=j+1; k<arr.size(); k++){
-                        if ( (abs(arr[j] - arr[k]) <= b) && (abs(arr[i] - arr[k]) <= c)){
+        for (int i=0; i<arr.size()-2; i++){
+            for (int k=i+2; k<arr.size(); k++){
+                if (abs(arr[i] - arr[k]) <= c){
+                    for (int j=i+1; j<k; j++){
+                        if ((abs(arr[j] - arr[k]) <= b) && (abs(arr[i] - arr[j]) <= a)){
                             count++;
                         }
                     }
                 }
-                // for (int k=j+1; k<arr.size(); k++){
-                //     if ((abs(arr[i] - arr[j]) <= a) && (abs(arr[j] - arr[k]) <= b) && (abs(arr[i] - arr[k]) <= c)){
-                //         count++;
-                //     }
-                // }
             }
         }
         return count;
