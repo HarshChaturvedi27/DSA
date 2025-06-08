a5eba1a19c152f3f555a41ed9ad7ff88d70d179c
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-08 21:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (52.72%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,21 +1,13 @@
 // class Solution {
 // public:
 //     vector<int> lexicalOrder(int n) {
-//         string temp = to_string(n);
-//         int a = temp[0];
-//         set <string> s;
-//         for (int i = 1; i <= n; i++){
+//         set<string> s;
+//         for (int i = 1; i <= n; i++) {
 //             s.insert(to_string(i));
 //         }
-//         vector <int> v;
-//         while (a>=0){
-//             a--;
-//             for (auto it : s){
-//                 if ((it)[0] == 1){
-//                     v.push_back(stoi(it));
-//                     s.erase(it);
-//                 }
-//             }
+//         vector<int> v;
+//         for (const auto& it : s) {
+//             v.push_back(stoi(it));
 //         }
 //         return v;
 //     }
@@ -24,13 +16,22 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        set<string> s;
-        for (int i = 1; i <= n; i++) {
-            s.insert(to_string(i));
-        }
-        vector<int> v;
-        for (auto it : s) {
-            v.push_back(stoi(it));
+        vector <int> v;
+        int curr = 1;
+        for (int i = 1 ; i <= n; i++){
+            v.push_back(curr);
+            if (curr * 10 <= n){
+                curr *= 10;
+            }
+            else {
+                if (curr >= n){
+                    curr /= 10;
+                }
+                curr++;
+                while (curr % 10 == 0){
+                    curr /= 10;
+                }
+            }
         }
         return v;
     }
