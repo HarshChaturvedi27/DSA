8a36eb353ccb32f287cc8f9a8adb036061e8dca9
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-21 03:13:18 +0530

    Time: 4 ms (18.97%) | Memory: 59.1 MB (90.81%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -7,7 +7,7 @@ public:
         for(int right = 0; right < nums.size(); right++) {
             if(nums[right] == 0) k--;
 
-            while(k < 0) {
+            if(k < 0) {
                 if(nums[left] == 0) k++;
                 left++;
             } 
