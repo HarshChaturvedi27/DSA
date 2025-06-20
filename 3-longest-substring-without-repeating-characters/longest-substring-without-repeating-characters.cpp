38ce0246690643c2b6600f0916127cc7a55d438f
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-20 23:38:55 +0530

    Time: 3 ms (84.16%) | Memory: 11.8 MB (58.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxi = 0, cnt = 0;
-        unordered_map <char, int> mpp;
-        string a;
-        for (auto it : s){
-            mpp[it]++;
-            if (mpp[it] == 2){
-                int idx = a.find(it);
-                for (int i = 0; i <= idx; i++){
-                    mpp[a[i]]--;
-                }
-                a.erase(0, idx +1);
+        // int maxi = 0, cnt = 0;
+        // unordered_map <char, int> mpp;
+        // string a;
+        // for (auto it : s){
+        //     mpp[it]++;
+        //     if (mpp[it] == 2){
+        //         int idx = a.find(it);
+        //         for (int i = 0; i <= idx; i++){
+        //             mpp[a[i]]--;
+        //         }
+        //         a.erase(0, idx +1);
+        //     }
+        //     a += it;
+        //     cnt = a.size();
+        //     maxi = max (maxi, cnt);
+        // }
+        // return maxi;
+
+        int res = 0;
+        unordered_map<char, int> lastOccurence;
+        int l = 0;
+
+        for (int r = 0; r < s.length(); ++r) {
+            char c = s[r];
+            if (lastOccurence.count(c)) {
+                l = max(l, lastOccurence[c] + 1);
             }
-            a += it;
-            cnt = a.size();
-            maxi = max (maxi, cnt);
+            lastOccurence[c] = r;
+            res = max(res, r - l + 1);
         }
-        return maxi;
+
+        return res;
+
     }
 };
