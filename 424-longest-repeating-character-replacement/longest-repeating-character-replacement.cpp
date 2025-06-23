45aa7ec4e3a0c69ddb0f3ffc00de4df1f6dce985
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-23 12:16:01 +0530

    Time: 1 ms (80.24%) | Memory: 11 MB (19.38%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,39 +1,21 @@
-// class Solution {
-// public:
-//     int characterReplacement(string s, int k) {
-//         unordered_map <char, int> mpp;
-//         int l = 0, maxi = INT_MIN;
-//         for (int r = 0; r < s.size(); r++){
-//             mpp[s[r]]++;
-//             if (mpp['A'] > k && mpp['B'] > k){
-//                 mpp[s[l]]--;
-//                 l++;
-//             }
-//             maxi = max(maxi, r -l +1);
-//         }
-//         return maxi;
-//     }
-// };
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char, int> count;
-        int left = 0;
-        int max_len = 0;
-        int max_count = 0;  
-        
-        for (int right = 0; right < s.size(); right++) {
-            count[s[right]]++;
-            max_count = max(max_count, count[s[right]]);
-            
-            while (right - left + 1 - max_count > k) {
-                count[s[left]]--;
-                left++;
+        int l = 0;
+        int maxCharCount = 0; // max occurence of any single char
+        int maxWin = 0;       // max valid window size
+        unordered_map <char, int> mpp;
+
+        for (int r = 0; r < s. size(); r++){
+            mpp[s[r]]++;
+            maxCharCount = max (maxCharCount, mpp[s[r]]);
+
+            if ( (r -l +1 - maxCharCount) > k ){
+                mpp[s[l]]--;
+                l++;
             }
-            
-            max_len = max(max_len, right - left + 1);
+            maxWin = max (maxWin, r -l +1);
         }
-        
-        return max_len;
+        return  maxWin;
     }
 };
