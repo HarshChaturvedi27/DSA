9764a9033c2215b0f20bc19d505a34758f414d9f
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-26 18:24:09 +0530

    Time: 8 ms (58.96%) | Memory: 71.4 MB (93.35%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,6 +1,7 @@
+// PREFIX SUM O(n), O(n)
+
 class Solution {
 public:
-
     int subCount(vector<int>& nums, int k){
         int cnt = 0, temp = 0, l = 0, r = 0;
         if (k < 0) return 0;
@@ -21,3 +22,22 @@ public:
         return ans;
     }
 };
+
+
+// PREFIX SUM O(n), O(n)
+// class Solution {
+// public:
+//     int numberOfSubarrays(vector<int>& nums, int k) {
+//         unordered_map<int, int> prefixCount;
+//         prefixCount[0] = 1;
+//         int count = 0, prefix = 0;
+
+//         for (int num : nums) {
+//             prefix += num % 2;
+//             count += prefixCount[prefix - k];
+//             prefixCount[prefix]++;
+//         }
+
+//         return count;
+//     }
+// };
