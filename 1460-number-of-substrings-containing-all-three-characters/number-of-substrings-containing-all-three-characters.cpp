ab8d85540575af5903d572800f0803395d9ead9e
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-29 03:16:50 +0530

    Time: 7 ms (68.53%) | Memory: 11 MB (56.39%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,44 @@
+// space O(n)
+// class Solution {
+// public:
+
+//     int minSubs(string& s, int k){
+//         unordered_map<char, int> mpp;
+//         int l = 0, r = 0, cnt = 0;
+//         while (r < s.size()){
+//             mpp[s[r]]++;
+//             while (mpp.size() > k){
+//                 mpp[s[l]]--;
+//                 if (mpp[s[l]] == 0) mpp.erase(s[l]);
+//                 l++;
+//             }
+//             cnt += (r -l +1);
+//             r++;
+//         }
+//         return cnt;
+//     }
+
+//     int numberOfSubstrings(string s) {
+//         int ans = minSubs(s, 3) - minSubs(s, 2);
+//         return ans;
+//     }
+// };
+
+// space O(1)
 class Solution {
 public:
-
-    int minSubs(string& s, int k){
-        unordered_map<char, int> mpp;
-        int l = 0, r = 0, cnt = 0;
-        while (r < s.size()){
-            mpp[s[r]]++;
-            while (mpp.size() > k){
-                mpp[s[l]]--;
-                if (mpp[s[l]] == 0) mpp.erase(s[l]);
-                l++;
+    int numberOfSubstrings(string s) {
+        int arr[3] = {-1, -1, -1};
+        int r = 0, cnt = 0;
+        while (r < s.length()){
+            arr[s[r] - 'a'] = r; // update last seen index of current char
+            int minIdx = INT_MAX;
+            for (int i = 0; i < 3; i++){
+                minIdx = min(minIdx, arr[i]);
             }
-            cnt += (r -l +1);
+            cnt += minIdx + 1;
             r++;
         }
         return cnt;
-    }
-
-    int numberOfSubstrings(string s) {
-        int ans = minSubs(s, 3) - minSubs(s, 2);
-        return ans;
     }
 };
