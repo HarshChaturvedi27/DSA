fe590476d2a181989608cacea2c48875a2f6293b
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-06-29 23:41:46 +0530

    Time: 1 ms (70.24%) | Memory: 8.6 MB (75.66%) - LeetSync

---
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
@@ -0,0 +1,65 @@
+// INCORRECT
+// class Solution {
+// public:
+//     int VowelSubstrings(string& word, int k) {
+//         int l = 0, r = 0, cnt = 0;
+//         unordered_map<char,int> mpp1;
+//         unordered_map<char,int> mpp2;
+//         while (r < word.length()){
+//             if (word[r] == 'a' || word[r] == 'e' || word[r] == 'i' || word[r] == 'o' || word[r] == 'u'){
+//                 mpp1[word[r]]++;
+//             }
+//             mpp2[word[r]]++;
+//             while (mpp1.size() != mpp2.size() && l <= r){
+//                 if (mpp1.count(word[l])) {
+//                     mpp1[word[l]]--;
+//                     if (mpp1[word[l]] == 0) mpp1.erase(word[l]);
+//                 }
+//                 mpp2[word[l]]--;
+//                 if (mpp2[word[l]] == 0) mpp2.erase(word[l]);
+//                 l++;
+//                 //if (mpp1.size() <= k) cnt++;
+//             }
+//             if (mpp1.size() <= k) {
+//                 cnt += r -l + 1;
+//             }
+//             r++;
+//         }
+//         return cnt;
+//     }
+//     int countVowelSubstrings(string word) {
+//         int ans = VowelSubstrings(word, 5) - VowelSubstrings(word, 4);
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    int VowelSubstrings(string& word, int k) {
+        int l = 0, r = 0, cnt = 0;
+        unordered_map<char,int> mpp;
+        while (r < word.length()){
+            if (word[r] == 'a' || word[r] == 'e' || word[r] == 'i' || word[r] == 'o' || word[r] == 'u'){
+                mpp[word[r]]++;
+            } else{
+                mpp.clear();
+                l = r +1;
+                r++;
+                continue;
+            }
+            while (mpp.size() > k){
+                mpp[word[l]]--;
+                if (mpp[word[l]] == 0) mpp.erase(word[l]);
+                l++;
+            }
+            cnt += r -l + 1;
+            r++;
+        }
+        return cnt;
+    }
+    int countVowelSubstrings(string word) {
+        int ans = VowelSubstrings(word, 5) - VowelSubstrings(word, 4);
+        return ans;
+    }
+};
+
