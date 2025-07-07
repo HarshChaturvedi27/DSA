4d7c5242fdce20605551c14e9eea3c415f51339d
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-08 01:03:37 +0530

    Time: 3 ms (54.48%) | Memory: 15.4 MB (13.72%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         vector<int> ans;
-        priority_queue <pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>>> minh;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minh;
         unordered_map<int,int> mpp;
 
-        for (auto it : nums){
+        for (auto it : nums) {
             mpp[it]++;
         }
-        for (auto it : mpp){
-            minh.push({it.second, -it.first}); // use '-' for taking greater ele above after converting back in minh when freq==
+        for (auto it : mpp) {
+            minh.push({it.second, -it.first}); // Using negative to handle descending order for same frequency
             // num = it.first
             // freq of num = it.second
-            // push(freq, num) coz we need to sort acc to freq
         }
-        while (minh.size()){
-            for (int i = 0; i < minh.top().first; i++){
-                ans.push_back(-minh.top().second); // convert '-' back to original
+        while (minh.size()) {
+            for (int i = 0; i < minh.top().first; i++) { // freq of num = minh.top().first
+                ans.push_back(-minh.top().second); // Convert back to positive // num = minh.top().first
             }
             minh.pop();
         }
