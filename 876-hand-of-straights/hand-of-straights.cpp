3573257cfba73dfeb1e3c38905fe2b5d9b0a092e
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-11 01:47:16 +0530

    Time: 39 ms (55.19%) | Memory: 32.4 MB (38.89%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        map <int, int> mpp;
-        vector<pair<int, int>> v;
-        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<int>> minh;
-
         if (hand.size() % groupSize != 0) return false;
 
-        for (int i = 0; i < hand.size(); i++){
-            mpp[hand[i]]++;
+        map<int, int> mpp;
+        for (int card : hand) {
+            mpp[card]++;
         }
-        for (auto it : mpp){
-            v.push_back(it);
-        }
-        while(!v.empty()){
-            vector<pair<int, int>> temp;
-            if (v.size() < groupSize) return false;
 
-            for (int i = 1; i < groupSize; i++){
-                if (v[i].first != (v[(i-1)].first + 1)) return false;
-            }
+        while (!mpp.empty()) {
+            int start = mpp.begin()->first;
 
-            // Decrement counts and store remaining
+            // Try to form a group starting from smallest key
             for (int i = 0; i < groupSize; i++) {
-                v[i].second--;
-                if (v[i].second > 0) {
-                    temp.push_back(v[i]);
-                }
+                int current = start + i;
+                if (mpp[current] == 0) return false;
+                mpp[current]--;
+                if (mpp[current] == 0) mpp.erase(current);
             }
-
-            // remove the used elements
-            v.erase(v.begin(), v.begin() + groupSize);
-            // Insert remaining counts back at the beginning
-            v.insert(v.begin(), temp.begin(), temp.end());
-            // Re-sort to maintain order (since we inserted at beginning)
-            // sort(v.begin(), v.end());
         }
+
         return true;
     }
 };
