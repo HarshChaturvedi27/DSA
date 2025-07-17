8307d60c0d64cdf1a70e54b0578c8bff6b0e5fdf
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-18 00:10:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (40.40%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -8,28 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// CORRECT BUT NOT BEST WAY OF WRITING
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if (head == NULL) return head;
+//         if (head->next == NULL) return head;
+//         if (head->next->next == NULL){
+//             ListNode* slow = head;
+//             ListNode* fast = head->next;
+//             slow->next = fast->next;
+//             fast->next = slow;
+//             return fast;
+//         }
+//         ListNode* slow = head;
+//         ListNode* fast = slow->next;
+//         ListNode* temp = fast->next;
+//         head->next = NULL;
+//         while (fast){
+//             fast->next = slow;
+//             slow = fast;
+//             fast = temp;
+//             if(temp != NULL) temp = temp->next;
+//         }
+//         return slow;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (head == NULL) return head;
-        if (head->next == NULL) return head;
-        if (head->next->next == NULL){
-            ListNode* slow = head;
-            ListNode* fast = head->next;
-            slow->next = fast->next;
-            fast->next = slow;
-            return fast;
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* temp = curr->next; // Store the next node
+            curr->next = prev;            // Reverse the current node's pointer
+            prev = curr;                  // Move prev to current node
+            curr = temp;                  // Move current to the next node
         }
-        ListNode* slow = head;
-        ListNode* fast = slow->next;
-        ListNode* temp = fast->next;
-        head->next = NULL;
-        while (fast){
-            fast->next = slow;
-            slow = fast;
-            fast = temp;
-            if(temp != NULL) temp = temp->next;
-        }
-        return slow;
+        return prev; // New head of the reversed list
     }
 };
