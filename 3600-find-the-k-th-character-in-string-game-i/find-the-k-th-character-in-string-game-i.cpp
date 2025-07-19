ae3a9e35212f10b66f65861aa7e108cfaf0b2ee5
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-19 15:08:02 +0530

    Time: 1 ms (54.58%) | Memory: 9.1 MB (49.01%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -21,32 +21,37 @@
 //     }
 // };
 
+// CORRECT
+// class Solution {
+// public:
+//     char kthCharacter(int k) {
+//         string s = "a";
+//         int r = 0;
+//         while (s.length() < k){
+//             int n = s.length();
+//             r = 0;
+//             while (r < n){
+//                 if (s[r] != 'z'){
+//                     char c = s[r] + 1;
+//                     s += c;
+//                 }
+//                 else s += 'a';
+//                 r++;
+//             }
+//         }
+//         return s[k-1];
+//     }
+// };
+
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s = "a";
-        int r = 0;
-        while (s.length() < k){
-            int n = s.length();
-            r = 0;
-            while (r < n){
-                if (s[r] != 'z'){
-                    char c = s[r] + 1;
-                    s += c;
-                }
-                else s += 'a';
-                r++;
-            }
+        string word = "a";
+        while (word.length() < k){
+            int curWordLength=word.length();
+            for (int i=0;i<curWordLength;++i)
+            word += (word[i]-'a'+1) % 26 + 'a';
         }
-        return s[k-1];
+        return word[k-1];
     }
 };
-
-// std::string word="a";
-//         while (word.length()<k)
-//         {
-//             int curWordLength=word.length();
-//             for (int i=0;i<curWordLength;++i)
-//             word+=(word[i]-'a'+1)%26+'a';
-//         }
-//         return word[k-1];
