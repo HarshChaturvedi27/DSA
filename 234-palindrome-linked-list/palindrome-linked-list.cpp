07bdf64957cc1dd7a66fe6652d067559f7ca9c9e
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-19 23:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 118.9 MB (58.90%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,30 +8,78 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        string s1, s2;
-        ListNode* slow = head;
-        ListNode* fast = head;
 
-        if (!head->next) return true;
-        if (!head->next->next){
-            return head->val == head->next->val;
-        }
+        //FINDING MID+1 OF LL
+        ListNode*fast = head;
+        ListNode*slow = head;
         while (fast && fast->next){
-            s1 += slow->val;
             slow = slow->next;
             fast = fast->next->next;
         }
-        while (slow){
-            s2 += slow->val;
-            slow = slow->next;
+
+        // REVERSING LL AFTER MID
+        ListNode*prev = NULL;
+        ListNode*curr = slow;
+        ListNode*next = curr;
+        while (curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        if (s1.length() != s2.length()){
-            s2.erase(0, 1);
+
+        // CHECKING FOR PALINDROME
+        ListNode* start = head;
+        ListNode* end = prev;
+        while(end){ // COZ END LL WILL EXHAUST FIRST & START LL HAS THE EXTRA ELEMENT
+            if (end->val != start->val) return false;
+            end = end->next;
+            start = start->next;
         }
-        reverse(s2.begin(), s2.end());
-        return s1 == s2;
+
+        // CORRECTING LL
+        curr = prev;
+        prev = NULL;
+        next = curr;
+        while (curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return true;
+
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         string s1, s2;
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+
+//         if (!head->next) return true;
+//         if (!head->next->next){
+//             return head->val == head->next->val;
+//         }
+//         while (fast && fast->next){
+//             s1 += slow->val;
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         while (slow){
+//             s2 += slow->val;
+//             slow = slow->next;
+//         }
+//         if (s1.length() != s2.length()){
+//             s2.erase(0, 1);
+//         }
+//         reverse(s2.begin(), s2.end());
+//         return s1 == s2;
+//     }
+// };
