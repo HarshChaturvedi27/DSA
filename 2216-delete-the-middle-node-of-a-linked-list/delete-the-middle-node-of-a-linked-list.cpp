68a7802bbb2d26fa153abcbbbb323385d5612daa
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-21 22:22:58 +0530

    Time: 8 ms (8.45%) | Memory: 312.1 MB (17.80%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -8,27 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         if (!head || !head->next) {
+            //delete head; //LC dosent support this
             return nullptr;
         }
         
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* slow = &dummy;
-        ListNode* fast = &dummy;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
         
-        while (fast->next && fast->next->next) {
+        while (fast && fast->next) {
+            prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
         
-        ListNode* toDelete = slow->next;
-        slow->next = slow->next->next;
-        delete toDelete;
+        if (prev) {
+            prev->next = slow->next;
+            delete slow;
+        } else {
+            // This case handles 2-node lists where we delete the first node
+            ListNode* newHead = head->next;
+            //delete head; // LC dosent support this
+            return newHead;
+        }
         
-        return dummy.next;
+        return head;
     }
 };
+
+// using dummy
+// class Solution {
+// public:
+//     ListNode* deleteMiddle(ListNode* head) {
+//         if (!head || !head->next) {
+//             return nullptr;
+//         }
+        
+//         ListNode dummy(0);
+//         dummy.next = head;
+//         ListNode* slow = &dummy;
+//         ListNode* fast = &dummy;
+        
+//         while (fast->next && fast->next->next) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+        
+//         ListNode* toDelete = slow->next;
+//         slow->next = slow->next->next;
+//         delete toDelete;
+        
+//         return dummy.next;
+//     }
+// };
