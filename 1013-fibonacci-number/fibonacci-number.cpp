d596bf34577030d85be55f17f04ba8c873de816f
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-07-28 21:32:35 +0530

    Time: 10 ms (46.22%) | Memory: 7.6 MB (87.60%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,3 +6,12 @@ public:
         return fib(n-1) + fib(n-2);
     }
 };
+
+// BETTER ANS BY VARUN
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n <= 1) return n;
+//         return fib(n-1) + fib(n-2);
+//     }
+// };
