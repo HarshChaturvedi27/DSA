2c83d7af35cd70a99ab797016d12f9d6675d1a82
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-08-17 21:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (41.80%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string s = to_string(num);
-        for (auto& it : s){
+        for (auto& it : s){ // necessary to write auto&
             if(it != '9'){
                 it = '9';
                 break;
