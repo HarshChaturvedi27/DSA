816352ba8cf91c08416f23060a52c5517a2257a9
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-08-29 23:45:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.52%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int x = 0;
+        while (pow(2, x) <= n){
+            if (pow(2, x) == n) return true;
+            x++;
+        }
+        return false;
+    }
+};
