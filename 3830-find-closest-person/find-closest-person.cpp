17ffc7501304b360ef1a3ef4c3ace7b9eaa59130
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-09-05 10:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (0.68%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -1,8 +1,16 @@
+// class Solution {
+// public:
+//     int findClosest(int x, int y, int z) {
+//         if (abs(x-z) < abs(y-z)) return 1;
+//         else if (abs(x-z) > abs(y-z)) return 2;
+//         return 0;
+//     }
+// };
+
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        if (abs(x-z) < abs(y-z)) return 1;
-        else if (abs(x-z) > abs(y-z)) return 2;
-        return 0;
+        if (abs(x-z) == abs(y-z)) return 0;
+        return (abs(x-z) < abs(y-z)) ? 1 : 2;
     }
 };
