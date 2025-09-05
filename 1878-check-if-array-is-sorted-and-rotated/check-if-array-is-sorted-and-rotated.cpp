f7dbccd07969e6bec7761d40250348dcbc2dc65c
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-09-05 12:26:02 +0530

    Time: 2 ms (3.23%) | Memory: 11.3 MB (13.17%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,20 +1,40 @@
+// Optimal n, 1
 class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        vector<int> copy = nums;
-        sort (copy.begin(), copy.end());
-        int x = 0;
-        for (int i = 0; i < n - 1; i++){
-            if (nums[i] > nums[i+1]) {
-                x = i+1; break;
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
             }
         }
-        for (int i = 0; i < n; i++){
-            if (copy[i] != nums[(i+x) % n]) {
-                return false;
-            }
-        }
-        return true;
+        
+        return count <= 1;
     }
 };
+
+
+// Correct n log n, n
+
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> copy = nums;
+//         sort (copy.begin(), copy.end());
+//         int x = 0;
+//         for (int i = 0; i < n - 1; i++){
+//             if (nums[i] > nums[i+1]) {
+//                 x = i+1; break;
+//             }
+//         }
+//         for (int i = 0; i < n; i++){
+//             if (copy[i] != nums[(i+x) % n]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
