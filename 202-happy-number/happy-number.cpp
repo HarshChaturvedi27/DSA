08e2aff6666096ff8c11e85b33b2adf6e3ec751b
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-09-13 00:07:47 +0530

    Time: 3 ms (18.90%) | Memory: 8.4 MB (22.90%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> visit;
-        
-        while (visit.find(n) == visit.end()) {
-            visit.insert(n);
-            n = getNextNumber(n);
-            if (n == 1) {
-                return true;
+        unordered_set<int> s;
+        while (n != 1){
+            if (s.find(n) != s.end()) return false;
+            
+            s.insert(n);
+            int sum = 0;
+            while (n > 0){
+                int digit = n%10;
+                sum += digit * digit;
+                // sum += (n%10 * n%10); //dosen't work gives wrong answer
+                n = n/10;
             }
+            n = sum;
         }
-        
-        return false;
-    }
-
-private:
-    int getNextNumber(int n) {
-        int output = 0;
-        
-        while (n > 0) {
-            int digit = n % 10;
-            output += digit * digit;
-            n = n / 10;
-        }
-        
-        return output;
+        return true;
     }
 };
