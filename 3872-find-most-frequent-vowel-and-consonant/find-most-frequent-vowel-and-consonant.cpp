78f1542ca49ea6ee0e686663acee0d32b62aed87
Harsh Chaturvedi <148485002+HarshChaturvedi27@users.noreply.github.com>
2025-09-13 15:08:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (44.54%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char, int> vowels;
-        unordered_map<char, int> cons;
+        unordered_map<char, int> char_cnt;
         int max_vowels = 0;
         int max_cons = 0;
         for (auto it : s){
             if (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u'){
-                vowels[it]++;
-                max_vowels = max(max_vowels, vowels[it]);
+                char_cnt[it]++;
+                max_vowels = max(max_vowels, char_cnt[it]);
             } else {
-                cons[it]++;
-                max_cons = max(max_cons, cons[it]);
+                char_cnt[it]++;
+                max_cons = max(max_cons, char_cnt[it]);
             }
         }
         return max_vowels + max_cons;
